9f2973bd6a635d36830a7f2a1b4ddccb81a043f2
Ugolovka <medusa_gangrena@tutamail.com>
2025-03-06 20:10:16 +0300

    maybe

--- octagon.py
+++ octagon.py
@@ -5,26 +5,35 @@ import numpy as np
 class Octagon:
     def __init__(self, side_length):
         self.side = side_length
-        self.angle = 135  
-        self.k = 1 + math.sqrt(2)
+        self.angle = 135  # Фиксированный угол (в градусах)
+        self.k = 1 + math.sqrt(2) # Константа k
+
     def circumcircle_radius(self):
-        ""
-        return self.side * self.k/2
+        "Радиус описанной окружности"
+        return self.side*self.k/2
+    
     def circumcircle_area(self):
-        ""
-        return math.pi * r **2
+        "Площадь описанной окружности"
+        r = self.circumcircle_radius()
+        return math.pi*r*2
+    
     def incircle_radius(self):
-        ""
+        "Радиус вписанной окружности"
         return self.side/(2*(math.sqrt(2)+1))
+    
     def incircle_area(self):
-        ""
-        return math.pi * r**2
+        "Площадь вписанной окружности"
+        r = self.incircle_radius()
+        return math.pi*r*2
+    
     def area(self):
-        ""
-        return 2*self.side**2(1+math.sqrt(2))
+        "Площадь октогона"
+        return 2*self.side**2*(1 + math.sqrt(2))
+    
     def perimetr(self):
-        ""
+        "Периметр октогона"
         return self.side*8
+    
     def draw(self):
         fig, ax = plt.subplots(figsize=(6, 6))
         ax.set_aspect('equal')
@@ -37,10 +46,10 @@ class Octagon:
         ax.fill(x, y, color='lightblue', label='Октогон')
 
         r_circ = self.circumcircle_radius()
-        circle_circ = plt.Circle((0,0), r_circ, color="", fill=False, label="Описанная окружность")
+        circle_circ = plt.Circle((0,0), r_circ, color="blue", fill=False, label="Описанная окружность")
         ax.add_patch(circle_circ)
         r_in = self.incircle_radius()
-        circle_in = plt.Circle((0,0), r_in, color="", fill=False, label="Вписаннная окружность")
+        circle_in = plt.Circle((0,0), r_in, color="red", fill=False, label="Вписаннная окружность")
         ax.add_patch(circle_in)
 
 
@@ -48,47 +57,12 @@ class Octagon:
         plt.grid()
         plt.show()
 
-        octagon = Octagon(5)
-        
+octagon = Octagon(10)
+print("Радиус описанной окружности", octagon.circumcircle_radius())
+print("Площадь описанной окружности", octagon.circumcircle_area())
+print("Радиус вписанной окружности", octagon.incircle_radius())
+print("Площадь вписанной окружности", octagon.incircle_area())
+print("Площадь октогона", octagon.area())
+print("Периметр октогона", octagon.perimetr())
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    def sounding(self):
-        print(f"Чик-чирик")
-    def eating(self):
-        print(f"птица {self.name} питается")
-    def sleeping(self):
-        print(f"птица {self.name} спит")
-    def flying_range(self,N):
-        if self.f_a==True:
-            print(self.size*N)
-        else:
-            print("Error")
-    def __del__(self):
-        print("dell done")
-
-vorobey= Birds("vorobey", True, 50)
-vorobey.flying_range(10)
-
-
-
-  def __init__(self, side_length):
-        self.side = side_length
-        self.angle = 135  # Фиксированный угол (в градусах)
-        self.k = 1 + math.sqrt(2)  # Константа k
+octagon.draw()
